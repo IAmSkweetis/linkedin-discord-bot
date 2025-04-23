24d088541a8a462b7ab770784311144bb48b022e
Scott Wolfe <scottawolfe@gmail.com>
2025-04-23 12:53:44 -0700

    Getting ready for a release workflow.

--- linkedin_discord_bot/cli/__main__.py
+++
@@ -1,3 +0,0 @@
-from linkedin_discord_bot.cli.main import top_level_cli
-
-top_level_cli(prog_name="Linkedin Discord Bot CLI")
--- linkedin_discord_bot/cli/version.py
+++ linkedin_discord_bot/cli/version.py
@@ -6,6 +6,6 @@ version_cli = typer.Typer(help="Commands related to the version.")
 
 
 @version_cli.command()
-def show_version() -> None:
+def version() -> None:
     """Print the version of the LinkedIn Discord Bot."""
     typer.secho(f"LinkedIn Discord Bot v{__version__}", fg=typer.colors.BLUE)
